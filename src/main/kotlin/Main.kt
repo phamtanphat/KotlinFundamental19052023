783506cc215dbf01062ac43b1a5833c8feaec06e
Phat <phatdroid94@gmail.com>
2023-05-31 18:38:07 +0700

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // Comment code : // => không chạy đoạn lệnh
+    // 1: Khai báo biến
+    // Từ khóa khai báo: var val
+    // Có 2 loại biến: Immutable(val), mutable(var)
+    // Cấu trúc: var tenbien = value
+    // var name = "Phạm Tấn Phát"
+    // val PI = 3.14
+}
