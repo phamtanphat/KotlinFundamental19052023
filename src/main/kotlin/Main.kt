577a583b1df81d2b53bc6fc5d35e171f7891b85b
Phat <phatdroid94@gmail.com>
2023-05-31 19:06:17 +0700

    2 - Type

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,21 @@
 fun main() {
     // Comment code : // => không chạy đoạn lệnh
+
     // 1: Khai báo biến
     // Từ khóa khai báo: var val
     // Có 2 loại biến: Immutable(val), mutable(var)
     // Cấu trúc: var tenbien = value
     // var name = "Phạm Tấn Phát"
     // val PI = 3.14
+
+    // 2: Kiểu dữ liệu
+    // Số nguyên: Int, Long (Số không có phần thập phân) => Example: -1 or 0 or 1
+    // Số thực: Float, Double (Số có phần thập phân) => Example: 1.2
+    // Kiểu chuỗi: String => Example: "Tèo"
+    // Kiểu ký tự: Char => Example: 'a'
+    // Kiểu mảng: Array
+    // Kiểu đối tượng: Object
+
+    var age: Int = 29
+    var height: Float = 1.75f
 }
